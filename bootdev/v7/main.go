664a4b91160d798b2c39900d40ced17fe32520d7
atanda0x <atandanafiu21@gmail.com>
2024-07-31 14:14:03 -0700

    const

---
+++ bootdev/v7/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const premiumPlanName = "Premium Plan"
+	basicPlanName := "Basic plan"
+
+	fmt.Println("premium plan", premiumPlanName)
+	fmt.Println("basic plan", basicPlanName)
+}
