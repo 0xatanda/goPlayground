039c3a2d38ab3885ae4ff15cfd0b845f13b475e7
atanda0x <atandanafiu21@gmail.com>
2024-07-31 13:55:39 -0700

    declaring variable

---
+++ bootdev/v2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var smsSentLimit int
+	var costPerSms float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %f %v %q \n", smsSentLimit, costPerSms, hasPermission, username)
+}
