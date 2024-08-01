6e186c0589553276cc9f70893387b924c40d83bd
atanda0x <atandanafiu21@gmail.com>
2024-07-31 17:36:18 -0700

    func

---
+++ bootdev/v11/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func concat(s1, s2 string) string {
+	return s1 + s2
+}
+
+func main() {
+	fmt.Println(concat("atanda", "0x"))
+	fmt.Println(concat("omowunmi", "idowu"))
+}
