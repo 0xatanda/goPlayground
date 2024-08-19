25d82a2c9e5120da144a305913d53670108a290a
0xatanda <atanda0x@gmail.com>
2024-08-19 13:05:34 -0700

    interfaces implementations

--- bootdev/v22/main.go
+++ bootdev/v22/main.go
@@ -1,6 +1,8 @@
 package main
 
-type emplotee interface {
+import "fmt"
+
+type employee interface {
 	getName() string
 	getSalary() int
 }
@@ -11,6 +13,40 @@ type contractor struct {
 	hourYrly int
 }
 
+type fullTime struct {
+	name   string
+	salary int
+}
+
 func (c contractor) getName() string {
-	return ""
+	return c.name
+}
+
+func (c contractor) getSalary() int {
+	return c.hourlyPy * c.hourYrly
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func test(e employee) {
+	fmt.Println(e.getName(), e.getSalary())
+	fmt.Println("========================================")
+}
+
+func main() {
+	test(fullTime{
+		name:   "0x",
+		salary: 50000,
+	})
+	test(contractor{
+		name:     "atanda",
+		hourlyPy: 100,
+		hourYrly: 73,
+	})
 }
