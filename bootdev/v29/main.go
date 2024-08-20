d112b1e083922a2fa26ec524dacaf6a17cd5fd36
0xatanda <atanda0x@gmail.com>
2024-08-19 18:38:47 -0700

    error pkg

---
+++ bootdev/v29/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(x, y float64) (float64, error) {
+	if y == 0 {
+
+	}
+	return x / y, nil
+}
+
+func test(x, y float64) {
+	defer fmt.Println("=========================")
+	fmt.Printf("Dividing %.2f by %.2f ..\n", x, y)
+	quotient, err := divide(x, y)
+	if err != nil {
+		errors.New("Cant be divided")
+	}
+	fmt.Println(quotient)
+}
+
+func main() {
+
+}
