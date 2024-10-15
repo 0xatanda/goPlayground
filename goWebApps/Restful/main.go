18a69977e03468276856dbbe70d399182cea4dc0
0xatanda <atanda0x@gmail.com>
2024-10-15 13:02:31 -0700

    post verb

---
+++ goWebApps/Restful/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"crypto/tls"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+const (
+	PORT    = ":8080"
+	DBHost  = "127.0.0.1"
+	DBPort  = ":3306"
+	DBUser  = "root"
+	DBPass  = "ethereum"
+	DBDbase = "cms"
+)
+
+var database *sql.DB
+
+type Page struct {
+	Title      string
+	Content    template.HTML
+	RawContent string
+	Date       string
+	GUID       string
+}
+
+type JSONResponse struct {
+	Fields map[string]string
+}
+
+func APIPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageGUID := vars["guid"]
+	thisPage := Page{}
+
+	// Correct SQL query with placeholder
+	err := database.QueryRow("SELECT page_title, page_content, page_date FROM pages WHERE page_guid=?", pageGUID).Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+	thisPage.Content = template.HTML(thisPage.RawContent)
+
+	if err != nil {
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		log.Println("Error fetching page:", err)
+		return
+	}
+
+	APIOutput, err := json.Marshal(thisPage)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Correct Content-Type
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(APIOutput))
+}
+func APICommentPost(w http.ResponseWriter, r *http.Request) {
+	var commentAdded bool
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	comments := r.FormValue("comments")
+	res, err := database.Exec("INSERT INTO comments SET comment_name=?, comment_email=?, comment_text=?", name, email, comments)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		commentAdded = false
+	} else {
+		commentAdded = true
+	}
+	commentAddedBool := strconv.FormatBool(commentAdded)
+	var resp JSONResponse
+	resp.Fields["id"] = string(id)
+	resp.Fields["added"] = commentAddedBool
+	jsonResp, _ := json.Marshal(resp)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, jsonResp)
+}
+
+func servePage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageGUID := vars["guid"]
+
+	thisPage := Page{}
+
+	// Correct SQL query with placeholder
+	err := database.QueryRow("SELECT page_title, page_content, page_date FROM pages WHERE page_guid=?", pageGUID).Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+	thisPage.Content = template.HTML(thisPage.RawContent)
+	if err != nil {
+		// Return 404 if the page is not found
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		log.Println("Couldn't get page:", err)
+		return
+	}
+
+	// Load template
+	t, err := template.ParseFiles("templates/blog.html")
+	if err != nil {
+		log.Println("Couldn't load template: ", err)
+		http.Error(w, "Couldn't load template", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with page data
+	err = t.Execute(w, thisPage)
+	if err != nil {
+		log.Println("Template execution error: ", err)
+		http.Error(w, "Error rendering page", http.StatusInternalServerError)
+	}
+}
+
+func ServeIndex(w http.ResponseWriter, r *http.Request) {
+	var Pages = []Page{}
+	pages, err := database.Query("SELECT page_title,page_content,page_date, page_guid FROM pages ORDER BY ? DESC", "page_date")
+	if err != nil {
+		fmt.Fprintln(w, err.Error())
+	}
+	defer pages.Close()
+	for pages.Next() {
+		thisPage := Page{}
+		pages.Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date, &thisPage.GUID)
+		thisPage.Content = template.HTML(thisPage.RawContent)
+		Pages = append(Pages, thisPage)
+	}
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, Pages)
+}
+
+func (p Page) TruncatedText() string {
+	chars := 0
+	for i := range p.Content {
+		chars++
+		if chars > 150 {
+			return string(p.Content[:i]) + `...`
+		}
+	}
+	return string(p.Content)
+}
+
+func RedirIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+}
+
+func main() {
+	dbConn := fmt.Sprintf("%s:%s@tcp(%s)/%s", DBUser, DBPass, DBHost, DBDbase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Panicln("Couldn't connect to db:", err)
+	}
+	database = db
+	defer db.Close()
+
+	routes := mux.NewRouter()
+
+	// Correct route and handler configuration
+	routes.HandleFunc("/", RedirIndex)
+	routes.HandleFunc("/home", ServeIndex)
+	routes.HandleFunc("/page/{guid:[0-9a-zA\\-]+}", servePage)
+	routes.HandleFunc("/api/pages", APIPage).Methods("GET").Schemes("https")
+	routes.HandleFunc("/api/pages/{guid:[0-9a-zA\\-]+}", APIPage).Methods("GET").Schemes("https")
+	routes.HandleFunc("/api/comments", APICommentPost).Methods("POST").Schemes("https")
+
+	// Start the server
+	http.Handle("/", routes)
+	fmt.Println("Server is running on port", PORT)
+	err = http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatal("Server error:", err)
+	}
+
+	certificates, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+	if err != nil {
+		log.Fatal("Error msg: ", err)
+	}
+	tlsConf := tls.Config{Certificates: []tls.Certificate{certificates}}
+	tls.Listen("tcp", PORT, &tlsConf)
+
+}
